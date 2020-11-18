0577e8f0834bb1d2f33d0c378e782961459f9443
Yasufumi Myoi <yasufumimyoi@yasufuminoMacBook-Air.local>
2020-11-18 19:58:49 +0900

    initial commit

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(5000, () => {
+  console.log("Sever has started!!");
+});
